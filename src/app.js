145b7f537467cb4488e39cadce0b0632c2ec4aaa
Juan1733 <juan.araque@correo.unimet.edu.ve>
2022-08-13 22:41:21 +0000

    ready

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function domainGenerator() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".us", ".io"];
+
+  let p = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let a = adj[Math.floor(Math.random() * adj.length)];
+  let n = noun[Math.floor(Math.random() * noun.length)];
+  let e = ext[Math.floor(Math.random() * ext.length)];
+
+  let name = p + a + n + e;
+  return name;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello World");
+  let h1 = document.querySelector("#domain");
+  h1.innerHTML = domainGenerator();
 };
